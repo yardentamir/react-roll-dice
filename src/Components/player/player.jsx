8495470fc5e78176aacabd21bd90b56c7a0ddf5e
yarentamir <yarden.tamir@walla.co.il>
2021-12-17 14:54:55 +0200

    total score and current score

--- src/Components/player/player.jsx
+++ src/Components/player/player.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./player.scss";
 
-export default function Player(props) {
-  return (<div className="player-div">
-    <h3>text</h3>
-    <div className=""></div>
+export default function Player({ id, totalScore, tempScore }) {
+  return (<div id={id} className="player-div">
+    <h3>Total Score</h3>
+    <div>{totalScore}</div>
+    <div><h3>current</h3>
+      <p>{tempScore}</p>
+    </div>
   </div>)
 
 }
--- src/Pages/DiceGame.jsx
+++ src/Pages/DiceGame.jsx
@@ -13,7 +13,7 @@ export default class DiceGame extends Component {
     super();
     this.state = {
       pointsToWin: 100,
-      dices: [null, null],
+      // dices: [null, null],
       gameOver: false,
       playersTurn: 0,
       players: [
@@ -28,30 +28,80 @@ export default class DiceGame extends Component {
       ]
     }
     this.handelDices = this.handelDices.bind(this);
+    this.tempScorePlayer = this.tempScorePlayer.bind(this);
+    this.dicesArr = [null, null];
   }
 
   handelDices() {
     for (let i = 0; i < 2; i++) {
-      this.setState((pervState) => {
-        return pervState.dices[i] = Math.floor(Math.random() * 6) + 1;
-      })
+      this.dicesArr[i] = Math.floor(Math.random() * 6) + 1;
+      // this.setState((pervState) => {
+      //   return pervState.dices[i] = Math.floor(Math.random() * 6) + 1;
+      // })
     }
-    this.setState({ dices: this.state.dices });
-    console.log(this.state.dices)
+    console.log(this.dicesArr);
+    if (this.dicesArr[0] === 6 && this.dicesArr[1] === 6) {
+      this.resetCurrentPlayerValues();
+      this.ChangeTurn();
+    } else {
+      this.totalScorePlayer(this.tempScorePlayer());
+    }
   }
 
   renderDices() {
-    return this.state.dices.map((dice, i) => {
-      console.log(dice)
+    return this.dicesArr.map((dice, i) => {
       return <Dice key={i} value={dice} />
     })
+  }
+
+  ChangeTurn() {
+    console.log(this.state.playersTurn);
+    this.setState((pervState) => {
+      return pervState.playersTurn = 1 - pervState.playersTurn;
+    });
+  }
+
+  totalScorePlayer(tempScorePlayer) {
+    this.setState((pervState) => {
+      return pervState.players[this.state.playersTurn].totalScore += tempScorePlayer;
+    });
+  }
+
+  tempScorePlayer() {
+    const tempScore = this.dicesArr.reduce((a, b) => a + b, 0);
+    this.setState((pervState) => {
+      return pervState.players[this.state.playersTurn].tempScore = tempScore;
+    });
+    return tempScore;
+  }
+
+  resetCurrentPlayerValues() {
+    this.setState((pervState) => {
+      return pervState.players[this.state.playersTurn].totalScore = 0;
+    });
+    this.setState((pervState) => {
+      return pervState.players[this.state.playersTurn].tempScore = 0;
+    });
+  }
+
+  resetGame() {
+    this.setState((pervState) => {
+      for (let i = 0; i < 2; i++) {
+        return pervState.players[i].totalScore = 0;
+      }
+    });
+    this.setState((pervState) => {
+      for (let i = 0; i < 2; i++) {
+        return pervState.players[i].tempScore = 0;
+      }
+    });
   }
 
 
   render() {
     return (
       <div className="flex-container">
-        <Player key="1" />
+        <Player key="0" id="0" totalScore={this.state.players[0].totalScore} tempScore={this.state.players[0].tempScore} />
         <div className="Columns-container">
           <NewGameButton />
           {this.renderDices()}
@@ -59,7 +109,7 @@ export default class DiceGame extends Component {
           <HoldAndDiceButtons callback={this.handelDices} text="HOLD" />
           <TopPointsInput />
         </div>
-        <Player key="2" />
+        <Player key="1" id="1" totalScore={this.state.players[1].totalScore} tempScore={this.state.players[1].tempScore} />
       </div>
     )
   }
